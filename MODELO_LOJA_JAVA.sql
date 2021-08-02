0a9c020643e657a070e858d6ef972ae272f64d19
Gustavo Pinto <64512499+gustavohpinto@users.noreply.github.com>
2021-08-01 21:59:51 -0300

    Base de dados

---
+++ MODELO_LOJA_JAVA.sql
@@ -0,0 +1,152 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 02-Ago-2021 às 02:58
+-- Versão do servidor: 10.1.37-MariaDB
+-- versão do PHP: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud`
+--
+CREATE DATABASE IF NOT EXISTS `crud` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `crud`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `idCli` int(11) NOT NULL,
+  `nomeCli` varchar(50) NOT NULL,
+  `endCli` varchar(100) NOT NULL,
+  `foneCli` varchar(15) NOT NULL,
+  `emailCli` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `clientes`
+--
+
+INSERT INTO `clientes` (`idCli`, `nomeCli`, `endCli`, `foneCli`, `emailCli`) VALUES
+(1, 'Junior', 'Rua Esmeralda, 390 - Bairro dos Bairros - Ribeirão Pires ', '11965463565', 'juninho@gmail.com'),
+(2, 'Cléber', 'Rua Lindinho, 77 - Centro - Ribeirão Pires', '11976346127', 'clebinho@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `ordem_servico`
+--
+
+CREATE TABLE `ordem_servico` (
+  `os` int(11) NOT NULL,
+  `data_os` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `equipamento` varchar(150) DEFAULT NULL,
+  `defeito` varchar(150) DEFAULT NULL,
+  `tecnico` varchar(50) DEFAULT NULL,
+  `valor` decimal(10,2) DEFAULT NULL,
+  `servico` varchar(150) DEFAULT NULL,
+  `idCli` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `ordem_servico`
+--
+
+INSERT INTO `ordem_servico` (`os`, `data_os`, `equipamento`, `defeito`, `tecnico`, `valor`, `servico`, `idCli`) VALUES
+(1, '2021-08-02 00:53:56', 'Computador Intel i5 - 4gb RAM - HDD 2TB', 'Computador não inicia vídeo', 'Gustavo', '150.00', 'Trocar pente de memória', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `idUsuario` int(11) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `fone` varchar(15) NOT NULL,
+  `login` varchar(50) NOT NULL,
+  `senha` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`idUsuario`, `usuario`, `fone`, `login`, `senha`) VALUES
+(1, 'Anderson Vanin', '11987348735', 'anderson', '1234');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`idCli`);
+
+--
+-- Indexes for table `ordem_servico`
+--
+ALTER TABLE `ordem_servico`
+  ADD PRIMARY KEY (`os`),
+  ADD KEY `idCli` (`idCli`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`idUsuario`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `idCli` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `ordem_servico`
+--
+ALTER TABLE `ordem_servico`
+  MODIFY `os` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `ordem_servico`
+--
+ALTER TABLE `ordem_servico`
+  ADD CONSTRAINT `ordem_servico_ibfk_1` FOREIGN KEY (`idCli`) REFERENCES `clientes` (`idCli`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
